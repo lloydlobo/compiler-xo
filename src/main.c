6f367ca82a6abfdd397ba0e4b8aacf8213ef55aa
Lloyd Lobo <lloydlobo4@gmail.com>
2023-10-18 03:26:08 +0530

    test: fix perf elapsed time to accumulate for laps
    instead of manually recording time elapsed for last
    run, we record it with _elapse helper function.
    This increments new elapsed time to previous one.
    Now, for example, for file I/O profiling, we can record
    total time it took, anywhere in the main function

--- src/main.c
+++ src/main.c
@@ -28,17 +28,17 @@
 #define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
 #define sizeof_field(t, f) (sizeof(((t *)0)->f))
 
-struct profiler_timer {
+struct perf_timer {
     double time_start;
     double time_end;
     double time_elapsed;
 };
-void profiler_timer_elapse(struct profiler_timer *self)
+void perf_timer_elapse(struct perf_timer *self)
 {
     self->time_elapsed
-        = ((double)(self->time_end - self->time_start)) / CLOCKS_PER_SEC;
+        += ((double)(self->time_end - self->time_start)) / CLOCKS_PER_SEC;
 };
-struct profiler {
+struct perf {
     struct {
         size_t number;
         char *label;
@@ -48,42 +48,41 @@ struct profiler {
         size_t total_lines_processed;
         size_t tokens_processed;
     } lexer;
-    struct profiler_timer front_end;
-    struct profiler_timer x64;
-    struct profiler_timer compiler;
-    struct profiler_timer linker;
-    struct profiler_timer total;
+    struct perf_timer front_end;
+    struct perf_timer x64;
+    struct perf_timer compiler;
+    struct perf_timer linker;
+    struct perf_timer total;
 };
-struct profiler profiler_init(size_t workspace_number, char *workspace_label)
+struct perf profiler_init(size_t workspace_number, char *workspace_label)
 {
-    struct profiler self;
+    struct perf self;
 
-    memset(&self, 0, sizeof(struct profiler));
+    memset(&self, 0, sizeof(struct perf));
 
     self.workspace.label = workspace_label;
     self.workspace.number = workspace_number;
 
     return self;
 }
-void profiler_start(struct profiler *self)
+void perf_start(struct perf *self)
 {
     self->total.time_start = clock();
 };
-void profiler_stop(struct profiler *self)
+void perf_stop(struct perf *self)
 {
     self->total.time_end = clock();
 
-    profiler_timer_elapse(&self->total);
-    profiler_timer_elapse(&self->linker);
-    profiler_timer_elapse(&self->compiler);
-    profiler_timer_elapse(&self->x64);
-    profiler_timer_elapse(&self->front_end);
+    perf_timer_elapse(&self->total);
+    perf_timer_elapse(&self->linker);
+    perf_timer_elapse(&self->compiler);
+    perf_timer_elapse(&self->x64);
+    perf_timer_elapse(&self->front_end);
 };
-void profiler_emit(const struct profiler *self)
+void perf_emit(const struct perf *self)
 {
-    printf("\n");
     printf(
-        "Stats for Workspace %zu (\"%s\"):\n",
+        "\nStats for Workspace %zu (\"%s\"):\n",
         self->workspace.number,
         self->workspace.label);
     printf(
@@ -92,22 +91,24 @@ void profiler_emit(const struct profiler *self)
         self->lexer.total_lines_processed);
     printf("Lexer tokens processed: %zu.\n", self->lexer.tokens_processed);
     printf("Front-end time: %.6f seconds.\n", self->front_end.time_elapsed);
-    printf("x64       time: %.6f seconds.\n", self->x64.time_elapsed);
-    printf("\n");
+    printf("x64       time: %.6f seconds.\n\n", self->x64.time_elapsed);
     printf("Compiler  time: %.6f seconds.\n", self->compiler.time_elapsed);
     printf("Link      time: %.6f seconds.\n", self->linker.time_elapsed);
-    printf("Total     time: %.6f seconds.\n", self->total.time_elapsed);
-    printf("\n");
+    printf("Total     time: %.6f seconds.\n\n", self->total.time_elapsed);
 };
 
 err_t parse_input_file_ext(const char *input, const char *expect_ext)
 {
     if (input == NULL || expect_ext == NULL)
         return ErrInvalid;
+
     size_t input_len = strlen(input), ext_len = strlen(expect_ext);
+
     if (input_len < ext_len + 1)
         return ErrBadName;
+
     const char *ext_check = input + (input_len - ext_len);
+
     if (strcmp(ext_check, expect_ext) != 0)
         return ErrBadfd;
 
@@ -124,11 +125,13 @@ int main(int argc, char *argv[])
         return EXIT_FAILURE;
     }
 
-    struct profiler profiler_handle;
-    profiler_handle = profiler_init(1, "Debug");
-    profiler_start(&profiler_handle);
+    struct perf perf_handle;
 
-    profiler_handle.front_end.time_start = clock();
+    perf_handle = profiler_init(1, "Debug");
+    perf_start(&perf_handle);
+    /* read input source file */
+
+    perf_handle.front_end.time_start = clock();
     const char *filename = argv[1];
     const char *expect_ext = "lum";
     err_t err = parse_input_file_ext(filename, expect_ext);
@@ -144,7 +147,6 @@ int main(int argc, char *argv[])
     fseek(input_file_lum, 0, SEEK_END);
     long input_size = ftell(input_file_lum);
     rewind(input_file_lum);
-
     char contents[input_size + 1]; // +1 for the null terminator
     {
         if (fread(&contents, sizeof(char), input_size, input_file_lum)
@@ -156,23 +158,19 @@ int main(int argc, char *argv[])
         contents[input_size] = '\0';
         fclose(input_file_lum);
     }
-    profiler_handle.front_end.time_end = clock();
-
+    perf_handle.front_end.time_end = clock();
     println("Hello, World from Lumina!");
-
-    profiler_handle.compiler.time_start = clock();
+    perf_handle.compiler.time_start = clock();
     /* module::tokenizer.c */
 
     int mut_token_count = 0;
     struct tokenizer *tokenizer = t_init(contents);
     struct token *tokens = t_tokenize(tokenizer, &mut_token_count);
-    profiler_handle.lexer.total_lines_processed
+    perf_handle.lexer.total_lines_processed
         = tokenizer->total_code_lines_processed;
-    profiler_handle.lexer.code_lines_processed
-        = tokenizer->code_lines_processed;
-    profiler_handle.lexer.tokens_processed = (size_t)mut_token_count;
+    perf_handle.lexer.code_lines_processed = tokenizer->code_lines_processed;
+    perf_handle.lexer.tokens_processed = (size_t)mut_token_count;
     t_free(tokenizer);
-
     /* module::parser.c */
 
     struct parser *parser = p_init(tokens, mut_token_count);
@@ -195,15 +193,15 @@ int main(int argc, char *argv[])
     g_free(&generator);
     p_free(parser);
     token_free_tokens(tokens, mut_token_count);
-    profiler_handle.compiler.time_end = clock();
+    perf_handle.compiler.time_end = clock();
     /* write assembly to output asm file */
 
     printf("\nGenerated Assembly Code:\n\n%s\n", output_asm);
 
-    profiler_timer_elapse(&profiler_handle.front_end);
-    clock_t clock_continued
-        = profiler_handle.front_end.time_elapsed * CLOCKS_PER_SEC;
-    profiler_handle.front_end.time_start = clock_continued;
+    perf_timer_elapse(&perf_handle.front_end); // record last
+    // clock_t clock_continued = perf_handle.front_end.time_end;
+    // perf_handle.front_end.time_start = clock_continued;
+    perf_handle.front_end.time_start = clock();
     {
         FILE *file_out_asm = fopen("out.asm", "w");
         if (file_out_asm == NULL)
@@ -211,34 +209,38 @@ int main(int argc, char *argv[])
         fputs(output_asm, file_out_asm);
         fclose(file_out_asm);
     }
-    profiler_handle.front_end.time_end = clock();
+    perf_handle.front_end.time_end = clock();
     /* assemble and... link the assembly code */
 
-    profiler_handle.x64.time_start = clock();
+    perf_handle.x64.time_start = clock();
     system("nasm -felf64 out.asm");
-    profiler_handle.x64.time_end = clock();
-    profiler_handle.linker.time_start = clock();
-    system("ld out.o -o out");
-    profiler_handle.linker.time_end = clock();
+    perf_handle.x64.time_end = clock();
 
-    profiler_stop(&profiler_handle);
-    profiler_emit(&profiler_handle);
+    perf_handle.linker.time_start = clock();
+    system("ld out.o -o out");
+    perf_handle.linker.time_end = clock();
+
+    perf_stop(&perf_handle);
+    perf_emit(&perf_handle);
 
     return EXIT_SUCCESS;
 
 fail_clean_file_out_asm_output_asm:
     perror("Could not open/create asm output file\n");
     free(output_asm);
+
     return EXIT_FAILURE;
 
 fail_clean_generator:
     perror("Could not generate assembly output\n");
     g_free(&generator);
+
     return EXIT_FAILURE;
 
 fail_clean_parser_tokens:
     fprintf(stderr, "--%d-- Invalid program\n", __LINE__);
     p_free(parser);
     token_free_tokens(tokens, mut_token_count);
+
     return EXIT_FAILURE;
 }
